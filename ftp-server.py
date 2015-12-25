b644844923bf793cdd5917dc69ac46e41f54344c
Ekky Melynda <ekkymelynda.ekky@gmail.com>
2015-12-25 12:02:04 +0700

    + PASV

--- ftp-server.py
+++ ftp-server.py
@@ -1,4 +1,3 @@
-#Tes git
 import socket
 import select
 import threading
@@ -7,6 +6,7 @@ import sys
 import time
 
 allow_delete = False
+local_ip = socket.gethostbyname(socket.gethostname())
 currdir = os.path.abspath('.')
 
 
@@ -120,33 +120,66 @@ class ftpserverfunc(threading.Thread):
 
 	#Mengganti nama file (RNTO: 4.1.3)
 	def RNTO(self,cmd):
-	        fn=os.path.join(self.cwd,cmd[5:-2])
-	        os.rename(self.rnfn,fn)
-	        self.conn.send('250 File renamed.\r\n')
+	    fn=os.path.join(self.cwd,cmd[5:-2])
+	    os.rename(self.rnfn,fn)
+	    self.client.send('250 File renamed.\r\n')
+
 	def RNFR(self,cmd):
-        	self.rnfn=os.path.join(self.cwd,cmd[5:-2])
-        	self.conn.send('350 Ready.\r\n')
+        self.rnfn=os.path.join(self.cwd,cmd[5:-2])
+        self.client.send('350 Ready.\r\n')
         
-        # Membuat direktori (MKD: 4.1.3)	
-        def MKD(self,cmd):
-	        dn=os.path.join(self.cwd,cmd[4:-2])
-	        os.mkdir(dn)
-	        self.conn.send('257 Directory created.\r\n')
+    #Membuat direktori (MKD: 4.1.3)
+    def MKD(self,cmd):
+	    dn=os.path.join(self.cwd,cmd[4:-2])
+	    os.mkdir(dn)
+	    self.client.send('257 Directory created.\r\n')
         
-        # Mendaftar file dan direktori (LIST: 4.1.3)
-        def LIST(self,cmd):
-	        self.conn.send('150 Here comes the directory listing.\r\n')
-	        print 'list:', self.cwd
-	        self.start_datasock()
-	        for t in os.listdir(self.cwd):
-	            k=self.toListItem(os.path.join(self.cwd,t))
-	            self.datasock.send(k+'\r\n')
-	        self.stop_datasock()
-	        self.conn.send('226 Directory send OK.\r\n')
-	        
+    #Mendaftar file dan direktori (LIST: 4.1.3)
+    def LIST(self,cmd):
+		self.client.send('150 Here comes the directory listing.\r\n')
+		print 'list:', self.cwd
+		self.start_datasock()
+		for t in os.listdir(self.cwd):
+			k=self.toListItem(os.path.join(self.cwd,t))
+			self.datasock.send(k+'\r\n')
+		self.stop_datasock()
+		self.client.send('226 Directory send OK.\r\n')
+
+	def PASV(self,cmd):
+		self.pasv_mode = True
+        self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.server.bind((local_ip,0))
+        self.server.listen(1)
+        ip, port = self.server.getsockname()
+        print 'open', ip, port
+        self.client.send('227 Entering Passive Mode (%s,%u,%u).\r\n' %
+                (','.join(ip.split('.')), port>>8&0xFF, port&0xFF))
+
+	def PORT(self,cmd):
+        if self.pasv_mode:
+            self.servsock.close()
+            self.pasv_mode = False
+        l=cmd[5:].split(',')
+        self.dataAddr='.'.join(l[:4])
+        self.dataPort=(int(l[4])<<8)+int(l[5])
+        self.conn.send('200 Get port.\r\n')
+
+	def start_datasock(self):
+        if self.pasv_mode:
+            self.datasock, addr = self.servsock.accept()
+            print 'connect:', addr
+        else:
+            self.datasock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            self.datasock.connect((self.dataAddr,self.dataPort))
+
+    def stop_datasock(self):
+        self.datasock.close()
+        if self.pasv_mode:
+            self.servsock.close()
+
 	# HELP: 4.1.3
 	def HELP(self,cmd):
-		self.conn.send('214-The following commands are recognized:\r\nABOR\r\n ADAT\r\nALLO\r\nAPPE\r\nAUTH\r\nCDUP\r\nCLNT\r\nCWD\r\nDELE\r\nEPRT\r\nEPSV\r\nFEAT\r\nHASH\r\nHELP\r\nLIST\r\nMDTM\r\n')
+		self.client.send('214-The following commands are recognized:\r\nABOR\r\n ADAT\r\nALLO\r\nAPPE\r\nAUTH\r\nCDUP\r\nCLNT\r\nCWD\r\nDELE\r\nEPRT\r\nEPSV\r\nFEAT\r\nHASH\r\nHELP\r\nLIST\r\nMDTM\r\n')
 		
 
 if __name__=='__main__':
